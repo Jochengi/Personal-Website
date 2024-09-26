78a7043ea5680b669f980541eb725c2c74a28a77
Jonathan Eng <jochengi77@gmail.com>
2024-09-25 21:39:41 -0400

    Transitions and back buttons

---
+++ page_transition.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const body = document.body;
+    body.classList.add('fade-in');
+    setTimeout(() => {
+        body.classList.add('show')
+    }, 150); // Small delay for fade class to apply before transition
+
+    const anchors = document.querySelectorAll('a');
+
+    anchors.forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+            e.preventDefault(); // Prevent immediate page navigation
+            document.body.classList.add('fade-out');
+            setTimeout(() => {
+                body.classList.add('hide')
+            }, 150); // Small delay for fade class to apply before transition
+
+            const href = this.href; // Target URL
+            
+            // Wait for fade-out transition to finish
+            setTimeout(function() {
+                window.location.href = href
+            }, 500);  // Match with CSS transition time
+        });
+    });
+});
